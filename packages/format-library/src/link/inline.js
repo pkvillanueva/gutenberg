4ddab04ccd8216909e36207b09c8095ec447e945
Alexander Botteram <a.botteram@gmail.com>
2019-06-12 14:50:36 +0200

    Removed superfluous className

--- packages/format-library/src/link/inline.js
+++ packages/format-library/src/link/inline.js
@@ -264,7 +264,6 @@ class InlineLinkUI extends Component {
 				onClickOutside={ this.onClickOutside }
 				onClose={ this.resetState }
 				focusOnMount={ showInput ? 'firstElement' : false }
-				className="editor-format-toolbar__link-settings-container"
 				renderSettings={ () => (
 					<div className="editor-format-toolbar__link-settings-container">
 						<Slot
